de6a20059253f9a02256880566b2fcadd584d9ce
Pawel Lipinski <lipinskijob@gmail.com>
2024-02-07 07:30:49 +0100

    Filesystem isolation

--- app/main.py
+++ app/main.py
@@ -1,13 +1,22 @@
 import subprocess
 import sys
+import tempfile
+import os
+import shutil
 
 
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
 
+    temp_path = tempfile.mkdtemp()
+    shutil.copy2(command, temp_path)
+    os.chroot(temp_path)
+
+    isolated_command = os.path.join("/", os.path.basename(command))
+
     completed_process = subprocess.Popen(
-        [command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        [isolated_command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
 
     stdout, stderr = completed_process.communicate()
