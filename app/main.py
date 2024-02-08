2370966490a5dc94dc394a7b0bcc23f442698a6e
Pawel Lipinski <lipinskijob@gmail.com>
2024-02-08 07:28:48 +0100

    Process isolation

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ import sys
 import tempfile
 import os
 import shutil
+# foreign function library
+import ctypes
 
 
 def main():
@@ -14,6 +16,15 @@ def main():
     os.chroot(temp_path)
 
     isolated_command = os.path.join("/", os.path.basename(command))
+
+    # man pid_namespaces
+    # loading the standard C library (libc.so.6)
+    # man libc. libc is just glibc
+    # man 2 unshare
+    libc = ctypes.cdll.LoadLibrary("libc.so.6")
+    # define CLONE_NEWPID	0x20000000	/* New pid namespace.  */
+    # sched.h
+    libc.unshare(0x20000000)
 
     completed_process = subprocess.Popen(
         [isolated_command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE
