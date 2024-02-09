29cf31cdf1a1ce4006fa724e86edfc20d8970bac
Pawel Lipinski <lipinskijob@gmail.com>
2024-02-09 07:37:06 +0100

    Fetch an image from the Docker Registry

--- app/main.py
+++ app/main.py
@@ -5,17 +5,57 @@ import os
 import shutil
 # foreign function library
 import ctypes
+import urllib.request
+import json
+import tarfile
 
 
 def main():
+    image = sys.argv[2]
     command = sys.argv[3]
     args = sys.argv[4:]
+    image_version = "latest"
+    image_name = image
+    if ":" in image:
+        image_name, image_version = image.split(":")
+
+    token_url = "https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/" + image_name + ":pull"
+
+    res = urllib.request.urlopen(token_url)
+    res_json = json.loads(res.read().decode())
+    token = res_json["token"]
+
+
+    manifest_url = "https://registry.hub.docker.com/v2/library/" + image_name + "/manifests/" + image_version
+
+    request = urllib.request.Request(
+        manifest_url,
+        headers={
+            "Accept": "application/vnd.docker.distribution.manifest.v2+json",
+            "Authorization": "Bearer " + token,
+        },
+    )
+    res = urllib.request.urlopen(request)
+    res_json = json.loads(res.read().decode())
+    layers = res_json["layers"]
 
     temp_path = tempfile.mkdtemp()
-    shutil.copy2(command, temp_path)
-    os.chroot(temp_path)
 
-    isolated_command = os.path.join("/", os.path.basename(command))
+    for layer in layers:
+        request = urllib.request.Request(
+            "https://registry.hub.docker.com/v2/library/"
+            + image_name
+            + "/blobs/"
+            + layer["digest"],
+            headers={"Authorization": "Bearer " + token},
+        )
+        res = urllib.request.urlopen(request)
+        tmp_file = os.path.join(temp_path, "file.tar")
+        with open(tmp_file, "wb") as f:
+            shutil.copyfileobj(res, f)
+        with tarfile.open(tmp_file) as tar:
+            tar.extractall(temp_path)
+        os.remove(tmp_file)
 
     # man pid_namespaces
     # loading the standard C library (libc.so.6)
@@ -26,8 +66,10 @@ def main():
     # sched.h
     libc.unshare(0x20000000)
 
+    os.chroot(temp_path)
+
     completed_process = subprocess.Popen(
-        [isolated_command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        [command, *args], stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
 
     stdout, stderr = completed_process.communicate()
